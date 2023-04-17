443fd9fcf8dce18859d3450e6e8ced58b48cd186
LiZheGuang <1030660726@qq.com>
2023-04-17 18:40:59 +0800

    feat: 🎸 upcdeo

---
+++ test1.py
@@ -0,0 +1,10 @@
+class main():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        print(self.name)
+        print(self.age)
+
+_man_play = main('zhangsan',18)
+
+print(_man_play)
