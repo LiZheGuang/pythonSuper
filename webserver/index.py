2b3ca88733ce671609f4146158571e3efc6b5503
LiZheGuang <1030660726@qq.com>
2023-04-23 17:35:11 +0800

    fix: 🐛 test code

--- webserver/index.py
+++ webserver/index.py
@@ -1,5 +1,6 @@
 from pymysql import Connection
 
+import json
 # sql参数
 conn = Connection(
     host="localhost",
@@ -21,7 +22,7 @@ conn.select_db('photo')
 
 
 # 使用游标对象-实例来执行SQL语句
-cursor.execute("SELECT photo_name,photo_name FROM  orders ;")
+cursor.execute("SELECT * FROM  orders ;")
 
 # 获取查询对象
 respose = cursor.fetchall()
@@ -29,14 +30,22 @@ respose = cursor.fetchall()
 
 
 # 打印转换后的数组JSON格式
-print(respose)
-# for item in respose:
-#     print(item)
+# 将结果转换为 JSON
+json_data = []
+for item in respose:
+    d = {}
+    d['name'] = item[0]
+    d['type'] = item[1]
+    d['path'] = item[2]
+
+    json_data.append(d)
 
 # 数据处理函数
 
+# 将 JSON 数据编码为字符串
+json_str = json.dumps(json_data)
 
-
+with open('result.json', 'w') as f: f.write(json_str)
 # 关闭
 conn.close()
 
